4254cca76482f6155c53ac60a9816953893da207
JeanRauta <j34nwr@gmail.com>
2024-10-31 09:49:55 -0300

    up

--- app/env.py
+++ app/env.py
@@ -1,4 +1,4 @@
-stringDB = "mongodb+srv://jean:gincana@teste.0nxua.mongodb.net/"
+stringDB = "mongodb+srv://cassiano3241:ReservasComeBem@reservascomebem.o62ji.mongodb.net/"
 nomeDB = "reservas_db"
 debug = True
 port = 5000
